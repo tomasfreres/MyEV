bf867c3870dc966a369cfc115e0078e34f24b15a
Tomás Freres <tomas@freres.xyz>
2020-12-07 17:12:30 +0100

    Initial Commit

---
+++ Shared/MyEVApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyEVApp.swift
+//  Shared
+//
+//  Created by Tomás Freres on 07.12.20.
+//
+
+import SwiftUI
+
+@main
+struct MyEVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
